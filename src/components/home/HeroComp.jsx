43c0aadc7bc64014c9ed79d9375232dcc57a4a10
iysaofficial <youngscientist.iysa@gmail.com>
2024-08-27 11:36:01 +0700

    close regis

--- src/components/home/HeroComp.jsx
+++ src/components/home/HeroComp.jsx
@@ -11,7 +11,7 @@ const HeroComp = () => {
                 </p>
                 <br />
                 <br />
-                <a href="https://registrasi-nypo.vercel.app/" target="_blank" className="registration-button">Registrasi</a>
+                <a href="#" className="registration-button">Tutup Registrasi</a>
                 
                 <div className="hero-social-icon">
                     <br />

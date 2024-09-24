e4a4caaf9b409f5af660d24144c17458dfe58fdd
iysaofficial <youngscientist.iysa@gmail.com>
2024-09-24 10:15:20 +0700

    upload list of winner 2024

--- src/components/NavbarComp.jsx
+++ src/components/NavbarComp.jsx
@@ -45,16 +45,27 @@ const NavbarComp = () =>{
                     <li className='menu-item'><a href="#about-section">Tentang</a></li>
                     {/* <li className='menu-item'><a href="/#category-main-section">Category</a></li> */}
                     {/* <li className='menu-item'><a href="#">FAQ</a></li> */}
-                    <li className='menu-item'><a href="#main-category-section">Kategori</a></li>
+                    {/* <li className='menu-item'><a href="#main-category-section">Kategori</a></li> */}
                     <li className='menu-item'><a href="https://drive.google.com/file/d/1mE_vdNwDjM9Kohc_0WlROf7Lkdm62tMZ/view?usp=sharing" target='_blank'>Buku Panduan</a></li>
-                    <li className='menu-item'><a href="#contact-section-wrapper">Kontak</a></li>
+                    {/* <li className='menu-item'><a href="#contact-section-wrapper">Kontak</a></li> */}
                     {/* <li className='menu-item'><a href="https://drive.google.com/drive/folders/1l_bCazIZmdtOSUjAuBAgXTg7YEBTixPY" target='_blank'>Certificate Supervisor</a></li> */}
 
-
+                    <li className='menu-item'>
+                        <a className='sub-btn' href="#">Daftar Pemenang <i className='fas fa-angle-down'></i></a>
+                        <ul className='sub-menu'>
+                            <li className='sub-item'><a href="https://drive.google.com/file/d/1AS-LH4HLaqT33iyC6c3ibZKd-lsP3U8x/view?usp=drive_link" target='_blank'>Daftar Pemenang 2024</a></li>
+                        </ul>
+                    </li> 
                     <li className='menu-item'>
                         <a className='sub-btn' href="#">Sertifikat Supervisor <i className='fas fa-angle-down'></i></a>
                         <ul className='sub-menu'>
                             <li className='sub-item'><a href="https://drive.google.com/drive/folders/1bYotKQV-jsxLIbsrlsFMpXsKh5EZc0y2?usp=drive_link" target='_blank'>Sertifikat Supervisor 2024</a></li>
+                        </ul>
+                    </li> 
+                    <li className='menu-item'>
+                        <a className='sub-btn' href="#">Galeri <i className='fas fa-angle-down'></i></a>
+                        <ul className='sub-menu'>
+                            <li className='sub-item'><a href="https://drive.google.com/drive/folders/1iazwpyafH3P0Zx4j0CXwuQ2J6mfADvql?usp=sharing" target='_blank'>2024</a></li>
                         </ul>
                     </li> 
                     {/* <li className='menu-item'>
--- src/components/home/HeroComp.jsx
+++ src/components/home/HeroComp.jsx
@@ -11,7 +11,7 @@ const HeroComp = () => {
                 </p>
                 <br />
                 <br />
-                <a href="#" className="registration-button">Tutup Registrasi</a>
+                <a href="#" className="registration-button">Segera Hadir 2025</a>
                 
                 <div className="hero-social-icon">
                     <br />

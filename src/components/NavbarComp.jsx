312057c01bc9511d578dd69fe6dde13cc5680365
iysaofficial <youngscientist.iysa@gmail.com>
2025-02-11 08:53:07 +0700

    galeri

--- src/components/NavbarComp.jsx
+++ src/components/NavbarComp.jsx
@@ -80,6 +80,7 @@ const NavbarComp = () =>{
                     <li className='menu-item'>
                         <a className='sub-btn' href="#">Galeri <i className='fas fa-angle-down'></i></a>
                         <ul className='sub-menu'>
+                            <li className='sub-item'><a href="https://drive.google.com/drive/folders/1UuGC30v3b9rhjYON-dP_1nz1NYdBrQhI?usp=sharing" target='_blank'>2025</a></li>
                             <li className='sub-item'><a href="https://drive.google.com/drive/folders/1iazwpyafH3P0Zx4j0CXwuQ2J6mfADvql?usp=sharing" target='_blank'>2024</a></li>
                         </ul>
                     </li> 
